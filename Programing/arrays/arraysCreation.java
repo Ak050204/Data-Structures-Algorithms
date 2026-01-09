13e52bff2cf9b9c64601397b8dc7cfd2dd52e7cb
Ak050204 <kandekaraniket05@gmail.com>
2026-01-09 10:41:14 +0530

    Create Multiple types of arrays

---
+++ Programing/arrays/arraysCreation.java
@@ -0,0 +1,10 @@
+package Programing.arrays;
+import java.util.*;
+
+public class arraysCreation {
+  public static void main(String[] args) {
+    int marks[] = new int[50]; // arrys size declered 50 but this is empty arrys and store the 0 every location
+    int numbers[]= {1,2,3,4,5,6}; //arrys size declered 6 is automaticly
+    String fruit[] = {"apple","banana","mango","grapes"}; //arrys size declered 4 is automaticly
+  }
+ }
