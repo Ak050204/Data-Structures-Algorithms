aa601030b1e714825d0e40c79e1dac075ebf3d45
Ak050204 <kandekaraniket05@gmail.com>
2026-01-10 10:33:28 +0530

    reverse Arrys

---
+++ Programing/arrays/reverseArray.java
@@ -0,0 +1,26 @@
+package Programing.arrays;
+
+public class reverseArray {
+
+  public static void reverseArrays(int arr[]){
+    int first = 0;
+    int last = arr.length -1;
+    while (first < last) {
+      int temp = arr[first];
+      arr[first] = arr[last];
+      arr[last] = temp;
+      first++;
+      last--;
+    }
+  }
+  public static void main(String[] args) {
+    int arr[] = {1, 2, 3, 4, 5};
+    reverseArrays(arr);
+    for(int i = 0; i < arr.length; i++){
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println(" this is reversed array");
+    }
+   
+    }
+  
