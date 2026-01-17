65b84bb59b246c8e68953f2af10737f40bd2c417
Ak050204 <kandekaraniket05@gmail.com>
2026-01-17 15:49:27 +0530

    Max subarrays find

---
+++ Programing/arrays/MaxSubArrays.java
@@ -0,0 +1,26 @@
+package Programing.arrays;
+
+public class MaxSubArrays {
+  public static void maxSubArraySum(int arr[]){
+    int maxSum = Integer.MIN_VALUE;
+    for(int i=0; i < arr.length; i++){
+      int start = i;
+      for(int j = i; j < arr.length; j++){
+        int end = j;
+        int currentSum = 0;
+        for(int k = i; k <= j; k++){
+          currentSum += arr[k];
+        }
+        if(currentSum > maxSum){
+          maxSum = currentSum;
+        }
+      }
+    }
+    System.out.println("Maximum Subarray Sum is: " + maxSum);
+  }
+  public static void main(String[] args) {
+    int arr[] = {1, -2, 6, -1, 3};
+    maxSubArraySum(arr);
+  }
+  
+}
