13e69a92b47b9d8b4940b3b6d1185f13b7e0c9ff
Ak050204 <kandekaraniket05@gmail.com>
2026-01-27 13:04:52 +0530

    DSA Sheet #4 Problem

---
+++ Project/homeWork/arrays1.java
@@ -0,0 +1,18 @@
+package Project.homeWork;
+
+public class arrays1 {
+  public boolean containsDuplicate(int nums[]){
+    for(int i = 0; i < nums.length; i++){
+      for(int j = i + 1; j < nums.length; j++){
+        if(nums[i] == nums[j]){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  public static void main(String[] args) {
+    int nums[]= {1, 2, 3, 4, 5, 7, 7};
+    System.out.println(new arrays1().containsDuplicate(nums));
+  }
+}
