70290f98918008470d528d17a39114dbd9b617bc
Ak050204 <kandekaraniket05@gmail.com>
2026-01-28 12:37:10 +0530

    create Inseration sort using while loop

---
+++ Programing/Sorting/inserationSort.java
@@ -0,0 +1,31 @@
+package Programing.Sorting;
+
+public class inserationSort {
+  public static void inserationSorting(int arr[]){
+    int n = arr.length;
+    for(int i = 0; i < n; i++){
+      int curr =arr[i];
+      int prev = i - 1;
+      while(prev >= 0 && arr[prev] > curr){
+        arr[prev+1] = arr[prev];
+        prev--;
+      }
+      // insertion
+      arr[prev+1]= curr;
+
+    }
+  }
+  // print array
+  public static void printArray(int arr[]){
+    for( int i = 0; i<arr.length; i++){
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+  public static void main(String[] args) {
+    int arr[] = {5, 4, 1, 3, 2};
+    inserationSorting(arr);
+    printArray(arr);
+
+  }
+}
