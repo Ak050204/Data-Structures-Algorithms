cce8779c7f3569b0407dfaef73f9cb98aaaa6341
Ak050204 <kandekaraniket05@gmail.com>
2026-02-13 09:56:36 +0530

    check no is even or odd by bit nos

---
+++ Programing/bitManuplation/oddEven.java
@@ -0,0 +1,17 @@
+package Programing.bitManuplation;
+
+public class oddEven {
+  public static void oddOrEven(int n){
+    int bitMask = 1;
+    if((n & bitMask )== 0){
+      System.out.println(n + " is an even number");
+    } else {
+      System.out.println(n + " is an odd number");
+    }
+  }
+  public static void main(String[] args) {
+    oddOrEven(3);
+    oddOrEven(4);
+    oddOrEven(17);
+  }
+}
