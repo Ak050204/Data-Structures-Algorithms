d380051d709102f206025c024ae318721b594e99
Ak050204 <kandekaraniket05@gmail.com>
2026-02-15 10:31:51 +0530

    Access modifier

---
+++ Programing/Oops/OOPS.java
@@ -0,0 +1,47 @@
+package Programing.Oops;
+
+public class OOPS {
+  public static void main(String[] args) {
+    Pen p1 = new Pen(); //Created a pen object called p1
+    p1.setColor("Blue");
+    System.out.println("Pen color: " + p1.color);
+    p1.setTip(5);
+    System.out.println("Pen tip: " + p1.tip);
+
+    BankAccount myAcc = new BankAccount();
+    myAcc.username = "GAK351";
+    myAcc.password = "12345"; // This will give an error because password is private
+  }
+}
+class Pen {
+  // Properties
+  String color;
+  int tip;
+// function
+  void setColor(String newColor){
+    color = newColor;
+  }
+  void setTip(int newTip){
+    tip = newTip;
+  }
+}
+
+class Student {
+  String name;
+  int age;
+  float percentage;
+
+  // function
+  void calcPercentage(int totalMarks, int marksObtaoned){
+    percentage = marksObtaoned * 100 / totalMarks;
+  }
+}
+
+class BankAccount {
+  public String username;
+  private String password;
+  public void setPassword(String pwd){
+    password = pwd;
+  }
+
+}
