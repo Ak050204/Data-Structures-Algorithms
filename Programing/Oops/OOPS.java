6815b309df901326258f73f8e70699bdc076c227
Ak050204 <kandekaraniket05@gmail.com>
2026-02-16 10:54:51 +0530

    getters and setters

--- Programing/Oops/OOPS.java
+++ Programing/Oops/OOPS.java
@@ -4,20 +4,28 @@ public class OOPS {
   public static void main(String[] args) {
     Pen p1 = new Pen(); //Created a pen object called p1
     p1.setColor("Blue");
-    System.out.println("Pen color: " + p1.color);
+    System.out.println("Pen color: " + p1.getColor());
     p1.setTip(5);
-    System.out.println("Pen tip: " + p1.tip);
+    System.out.println("Pen tip: " + p1.getTip());
+    p1.setColor("Red");
+    System.out.println("Pen color: " + p1.getColor());
 
-    BankAccount myAcc = new BankAccount();
-    myAcc.username = "GAK351";
-    myAcc.password = "12345"; // This will give an error because password is private
+    // BankAccount myAcc = new BankAccount();
+    // myAcc.username = "GAK351";
+    // myAcc.password = "12345"; // This will give an error because password is private
   }
 }
 class Pen {
   // Properties
-  String color;
-  int tip;
+  private String color;
+  private int tip;
 // function
+String getColor(){
+  return this.color;
+}
+int getTip(){
+  return this.tip;
+}
   void setColor(String newColor){
     color = newColor;
   }
