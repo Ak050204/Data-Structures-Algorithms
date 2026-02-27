0f190722a4d5bd2a744233315f5ccb50e58b4390
Ak050204 <kandekaraniket05@gmail.com>
2026-02-27 10:20:07 +0530

    increasing order

--- Programing/Recursion/RecursionBasics.java
+++ Programing/Recursion/RecursionBasics.java
@@ -8,9 +8,19 @@ public class RecursionBasics {
     }
     System.out.println(n);
     printDec(n-1);
+}
+    public static void printInc(int n){
+      if(n == 10){
+        System.out.print(n + " ");
+        return;
+      }
+      System.out.print(n + " ");
+      printInc(n+1);
+      
+    
   }
   public static void main(String[] args) {
-    int n = 10;
-    printDec(n);
+    int n = 1;
+    printInc(n);
   }
 }
