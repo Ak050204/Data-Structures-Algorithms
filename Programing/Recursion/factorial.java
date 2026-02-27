e53ffbab38add82b58f8f6dba2ee819bb90924b3
Ak050204 <kandekaraniket05@gmail.com>
2026-02-27 11:20:19 +0530

    find the factrial no

---
+++ Programing/Recursion/factorial.java
@@ -0,0 +1,17 @@
+package Programing.Recursion;
+
+public class factorial {
+  public static int factorials(int n)
+  {
+    if(n == 0){
+      return 1;
+    }
+    int fam1 = factorials(n-1);
+    int fact = n * factorials(n-1);
+    return fact;
+  }
+  public static void main(String[] args) {
+    int n = 5;
+System.out.println(factorials(n));
+  }
+}
