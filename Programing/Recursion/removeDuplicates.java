af3fa11870822c17d3bba2729a0446f0ce0372fc
Ak050204 <kandekaraniket05@gmail.com>
2026-03-20 10:05:11 +0530

    remove duplicates

---
+++ Programing/Recursion/removeDuplicates.java
@@ -0,0 +1,26 @@
+package Programing.Recursion;
+
+public class removeDuplicates {
+  public static void removeDup(String str, int idx, StringBuilder newStr, boolean map[]){
+    if(idx == str.length()){
+      System.out.println(newStr);
+      return;
+    }
+    // kaam
+    char currChar = str.charAt(idx);
+    if(map[currChar-'a'] == true){
+      // duplicate
+      removeDup(str, idx+1, newStr, map);
+    }else {
+      // not duplicate
+      newStr.append(currChar);
+      map[currChar-'a'] = true;
+      removeDup(str, idx+1, newStr, map);
+    }
+  }
+  public static void main(String[] args){
+    String str = "aniketbalasahebkandekar";
+    // boolean[] map = new boolean[26];
+    removeDup(str, 0, new StringBuilder(""), new boolean[26]);
+  }
+}
