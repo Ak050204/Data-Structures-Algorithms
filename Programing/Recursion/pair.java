bb7402eb586fb31b55b1a3d25b0e4eb9847d03c1
Ak050204 <kandekaraniket05@gmail.com>
2026-04-10 10:43:10 +0530

    pair of friends

---
+++ Programing/Recursion/pair.java
@@ -0,0 +1,22 @@
+package Programing.Recursion;
+
+public class pair {
+  public static int friendspairing(int n){
+    if(n == 1 || n == 2){
+      return n;
+    }
+    // choice
+    // single
+    // int fnm1 = friendspairing(n-1);
+    // pair
+    // int fnm2 = friendspairing(n-2);
+    // int pairWays = (n-1)*fnm2;
+
+    // return fnm1 + pairWays;
+
+    return friendspairing(n-1) + (n-1)*friendspairing(n-2);
+  }
+  public static void main(String[] args) {
+    System.out.println(friendspairing(4));
+  }
+}
